8f10ed3f2609e1ef7cb16dff9e3cd3635bac08b4
SorinEni <60381859+SorinEni@users.noreply.github.com>
2020-02-25 10:47:56 +0100

    Create index.php
    6 proměnných

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+$A = "20";
+$B = "2";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <p> A = <?= $A; ?> </p>
+    <p> B = <?= $B; ?> </p> 
+    <p>Součet: <?= $A + $B; ?> </p>
+    <p>Součin: <?= $A * $B; ?> </p>
+    <p>Rozdíl: <?= $A - $B; ?> </p>
+    <p>Podíl: <?= $A / $B; ?> </p>
+
+
+</body>
+</html>
